0c03a0a6705fc1ff8273d437bbabdee224938887
Saalim Quadri <danascape@gmail.com>
2024-08-20 22:38:05 +0530

    fs: fuse: Revert OnePlus shortcircuit Import
    * In stock kernel tree, oneplus's own shortcircuit implementation was
      working, which conflicts with passthrough impl from ACK Upstream.
      So remove this and embrace passthrough :)
    
    Change-Id: If98fb66a085b5aae18c5b031999b61abc49907f5
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2304,9 +2304,6 @@ static __latent_entropy struct task_struct *copy_process(
 #ifdef CONFIG_TPP
 		p->tpp_flag = 0;
 #endif
-#ifdef CONFIG_ONEPLUS_FG_OPT
-		p->fuse_boost = 0;
-#endif
 #ifdef CONFIG_IM
 		im_tsk_init_flag((void *) p);
 #endif
